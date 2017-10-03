3ec96ef261b652478af88f2d5de619228f5de292
Olivier Hachette <hit_@hotmail.fr>
2017-10-03 19:43:47 +0000

    Put back scene in TP2

--- TP_Rendu/Surface.js
+++ TP_Rendu/Surface.js
@@ -1,3 +1,4 @@
+/*global THREE*/
 class Surface{
     constructor(pointsArray, lineGenerator){
         this.pointsArray = Surface.matrix(pointsArray.length,pointsArray[0].length);
@@ -6,12 +7,12 @@ class Surface{
                 this.pointsArray[k][l] =  pointsArray[k][l].clone();
             
         this.resolution = {'x':0,'y':0};
-        this.points = null;
+        this.points = [];
         this.lineGenerator = lineGenerator;
     }
     
     computePoints(x,y,order){
-        this.points = Surface.matrix(x,y);
+        this.points = [];
         var directrice = [];
         for(var k in this.pointsArray){
             directrice[k] = new this.lineGenerator(this.pointsArray[k],order).getLine(x);
@@ -23,7 +24,7 @@ class Surface{
             }
             var generatrice = new this.lineGenerator(dir,order).getLine(y);
             for(var j = 0; j < y; j++){
-                this.points[i][j]=generatrice[j];
+                this.points.push(generatrice[j]);
             }
         }
         return this.points;
@@ -38,6 +39,25 @@ class Surface{
             return this.points;
         this.resolution = resolution;
         return this.computeSurface(resolution.x, resolution.y, resolution.order);
+    }
+    
+    getSurfaceGeometry(resolution){
+        this.getSurface(resolution);
+        var geom = new THREE.Geometry();
+        geom.vertices = geom.vertices.concat(this.points);
+        var l = resolution.x;
+        for(var j = 0; j < resolution.y - 1; j++){
+            for(var i = 0; i < resolution.x - 1; i++){
+                geom.faces.push( new THREE.Face3( i + j*l, i + 1 + j*l, i + l + j*l ) );
+                geom.faces.push( new THREE.Face3( i + l +  j*l, i + 1 + j*l, i + l + 1 + j*l ) );
+            }
+        }
+        for(var k in geom.faces){
+            geom.faces[k].normal.set(0, 0, 1);
+        }
+        
+        console.log(geom)
+        return geom;
     }
     
     static matrix(m, n) {
