b9f18a1f7075aed917a72d80b356f8c43126874c
Canardou <hit_@hotmail.fr>
2017-10-22 22:15:47 +0200

    Correction TP Signal

--- TP_Signal/TP2/spectrogramme.m
+++ TP_Signal/TP2/spectrogramme.m
@@ -5,9 +5,9 @@ t=0:1/Fs:tf-1/Fs
 f1=0;
 f2=150; % start @ 0 Hz, go up to 150Hz
 semi_t=0:1/Fs:(tf/2-1/Fs);
-sl=2*(f2-f1/2);
+sl=2*((f2-f1)/2);
 f1=f1*semi_t+(sl.*semi_t/2);
-f2=f2+f2*semi_t-sl.*semi_t/2;
+f2=f2+(f2*semi_t-sl.*semi_t)/2;
 f=[f1 f2];
 y=cos(2*pi*f.*t);
 plot(t,y);
