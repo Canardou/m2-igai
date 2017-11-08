19acedd5648e1d2930fac9d463eb498f9fc89f74
hachette olivier <hcl0216a@112-16l.ens.fsi>
2017-11-08 15:56:19 +0100

    TP Vison :rat:

--- TP_Vision/response.m
+++ TP_Vision/response.m
@@ -1,23 +1,26 @@
 function [R]=response(Im,SigmaDerivation,SigmaIntegration,Method)
-    [Smooth,d1,d2,d3,d4,d5] = gaussmask2(SigmaIntegration);
-    [s,d6,d7,DerivII,DerivJJ,DerivIJ] = gaussmask2(SigmaDerivation);
-    mu11 = conv2(conv2(Im,Smooth,'same'),DerivII,'same');
-    mu22 = conv2(conv2(Im,Smooth,'same'),DerivJJ,'same');
-    mu12 = conv2(conv2(Im,Smooth,'same'),DerivIJ,'same');
-    [h, w] = size(Im);
-    for i = 1:h
-        for j = 1:w
-            M = [mu11(i,j) mu12(i,j); mu12(i,j) mu22(i,j)];
-            if strcmp(Method,'Harris-Plessey')
-                R(i,j) = det(M)-0.04*trace(M)*trace(M);
-            elseif strcmp(Method,'Noble')
-                R(i,j) = 2*det(M)/(trace(M)+10^-15);
-            elseif strcmp(Method,'Shi-Tomasi')
-                a = M(1,1);
-                b = M(1,2);
-                c = M(2,1);
-                R(i,j) = (a + c - sqrt((a-c)^2+4*b*b))/2;
-            end
-        end
+    [d1,di,dj,d4,d5,d6] = gaussmask2(SigmaDerivation);
+    
+    DerivI = conv2(Im,di,'same');
+    DerivJ = conv2(Im,dj,'same');
+    mu11 = DerivI.^2;
+    mu22 = DerivJ.^2;
+    mu12 = DerivI.*DerivJ;
+    
+    Smooth = gaussmask2(SigmaIntegration);
+    UniformSmooth = ones(size(Smooth))/numel(Smooth);
+    mu11 = conv2(mu11,Smooth,'same');
+    mu22 = conv2(mu22,Smooth,'same');
+    mu12 = conv2(mu12,Smooth,'same');
+    
+    shitmu11 = conv2(mu11,UniformSmooth,'same');
+    shitmu12 = conv2(mu12,UniformSmooth,'same');
+    
+    if strcmp(Method,'Harris-Plessey')
+        R = mu11.*mu22-mu12.^2-0.04*(mu11+mu22).^2;
+    elseif strcmp(Method,'Noble')
+        R = 2*(mu11.*mu22-mu12.^2)./(mu11+mu22+10^-15);
+    elseif strcmp(Method,'Shi-Tomasi')
+        R = (shitmu11 + shitmu12 - sqrt((shitmu11-shitmu12).^2+4*shitmu12.^2))/2;
     end
 end
